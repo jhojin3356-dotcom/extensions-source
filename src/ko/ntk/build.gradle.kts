0223e7242a67b5e90c8a17296b6d33dcf4ed951d
jhojin3356-dotcom <jhojin3356@gmail.com>
2026-07-05 22:05:34 +0900

    Add build.gradle.kts for NTK plugin configuration

---
+++ src/ko/ntk/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("kei.plugins.extension")
+}
+
+keiyoushi {
+    name.set("NTK")
+    className.set(".NTKFactory")
+    libVersion.set("1.4")
+    versionCode.set(1)
+    isNsfw.set(true)
+}
