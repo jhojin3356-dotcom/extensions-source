5eee91768a0344f25fbe2cc62205a04d483f98fd
jhojin3356-dotcom <jhojin3356@gmail.com>
2026-07-05 21:15:19 +0900

    Update build.gradle.kts

--- src/ko/ntk/build.gradle.kts
+++ src/ko/ntk/build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
-    id("kei.plugins.multisrc")
+    id("kei.plugins.extension")
+}
+
+keiyoushi {
+    name.set("NTK")
+    className.set(".NTKFactory")
+    libVersion.set("1.4")
+    versionCode.set(1)
+    isNsfw.set(true)
 }
