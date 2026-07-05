cc5c200caa1971b566520537ef6a37ca42644ad2
jhojin3356-dotcom <jhojin3356@gmail.com>
2026-07-05 21:00:46 +0900

    Update build.gradle.kts

--- src/ko/ntk/build.gradle.kts
+++ src/ko/ntk/build.gradle.kts
@@ -1,11 +1,3 @@
 plugins {
-    id("eu.kanade.tachiyomi.extension")
-}
-
-ext {
-    extName = "NTK"
-    pkgNameSuffix = "ko.ntk"
-    extClass = ".NTKFactory"
-    extVersionCode = 1
-    isNsfw = true
+    id("kei.plugins.multisrc")
 }
