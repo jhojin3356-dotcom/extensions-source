10ea7e8cc1a751650ca17a2eb86a1582230cf302
jhojin3356-dotcom <jhojin3356@gmail.com>
2026-07-05 19:40:16 +0900

    Update NTKBase.kt

--- src/ko/ntk/NTKBase.kt
+++ src/ko/ntk/NTKBase.kt
@@ -1,1 +1,27 @@
+package eu.kanade.tachiyomi.extension.ko.ntk
 
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.NetworkHelper
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import uy.kohesive.injekt.injectLazy
+
+open class NTKBase {
+
+    protected val network: NetworkHelper by injectLazy()
+    protected val client: OkHttpClient
+        get() = network.client
+
+    protected val baseUrl = "https://newtoki1.org"
+
+    protected fun request(path: String): Request {
+        return GET(baseUrl + path)
+    }
+
+    protected fun getDocument(path: String): Document {
+        val response = client.newCall(request(path)).execute()
+        return Jsoup.parse(response.body.string())
+    }
+}
